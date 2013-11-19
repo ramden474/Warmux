bd53147ff85352334b3693f06b3b735a604a11c1
xavier <xavierhd@hotmail.com>
2013-11-19 18:48:05 -0500

    Version compilable definitivement

---
+++ warmux-11.04.1+repack/lib/warmux/include/config_autotools.h
@@ -0,0 +1,171 @@
+/* lib/warmux/include/config_autotools.h.  Generated from config_autotools.h.in by configure.  */
+/* lib/warmux/include/config_autotools.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
+#define ENABLE_NLS 1
+
+/* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
+   CoreFoundation framework. */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+
+/* Define to 1 if you have the MacOS X function CFPreferencesCopyAppValue in
+   the CoreFoundation framework. */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+
+/* Define if the GNU dcgettext() function is already present or preinstalled.
+   */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
+   */
+#define HAVE_DIRENT_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define if you have the iconv() function and it works. */
+/* #undef HAVE_ICONV */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have a functional curl library. */
+/* #undef HAVE_LIBCURL */
+
+/* Define to 1 if you have the `SDL_gfx' library (-lSDL_gfx). */
+#define HAVE_LIBSDL_GFX 1
+
+/* Define to 1 if you have the `SDL_image' library (-lSDL_image). */
+#define HAVE_LIBSDL_IMAGE 1
+
+/* Define to 1 if you have the `SDL_mixer' library (-lSDL_mixer). */
+#define HAVE_LIBSDL_MIXER 1
+
+/* Define to 1 if you have the `SDL_net' library (-lSDL_net). */
+#define HAVE_LIBSDL_NET 1
+
+/* Define to 1 if you have the `SDL_ttf' library (-lSDL_ttf). */
+#define HAVE_LIBSDL_TTF 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
+/* #undef HAVE_NDIR_H */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Defined if libcurl supports AsynchDNS */
+/* #undef LIBCURL_FEATURE_ASYNCHDNS */
+
+/* Defined if libcurl supports IDN */
+/* #undef LIBCURL_FEATURE_IDN */
+
+/* Defined if libcurl supports IPv6 */
+/* #undef LIBCURL_FEATURE_IPV6 */
+
+/* Defined if libcurl supports KRB4 */
+/* #undef LIBCURL_FEATURE_KRB4 */
+
+/* Defined if libcurl supports libz */
+/* #undef LIBCURL_FEATURE_LIBZ */
+
+/* Defined if libcurl supports NTLM */
+/* #undef LIBCURL_FEATURE_NTLM */
+
+/* Defined if libcurl supports SSL */
+/* #undef LIBCURL_FEATURE_SSL */
+
+/* Defined if libcurl supports SSPI */
+/* #undef LIBCURL_FEATURE_SSPI */
+
+/* Defined if libcurl supports DICT */
+/* #undef LIBCURL_PROTOCOL_DICT */
+
+/* Defined if libcurl supports FILE */
+/* #undef LIBCURL_PROTOCOL_FILE */
+
+/* Defined if libcurl supports FTP */
+/* #undef LIBCURL_PROTOCOL_FTP */
+
+/* Defined if libcurl supports FTPS */
+/* #undef LIBCURL_PROTOCOL_FTPS */
+
+/* Defined if libcurl supports HTTP */
+/* #undef LIBCURL_PROTOCOL_HTTP */
+
+/* Defined if libcurl supports HTTPS */
+/* #undef LIBCURL_PROTOCOL_HTTPS */
+
+/* Defined if libcurl supports LDAP */
+/* #undef LIBCURL_PROTOCOL_LDAP */
+
+/* Defined if libcurl supports TELNET */
+/* #undef LIBCURL_PROTOCOL_TELNET */
+
+/* Defined if libcurl supports TFTP */
+/* #undef LIBCURL_PROTOCOL_TFTP */
+
+/* Name of package */
+#define PACKAGE "warmux"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "warmux-dev@gna.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Warmux"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Warmux 11.04"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "warmux"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "11.04"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to use FriBidi */
+/* #undef USE_FRIBIDI */
+
+/* Define to use X11 copy'n'paste */
+#define USE_X11 1
+
+/* Version number of package */
+#define VERSION "11.04"
+
+/* Define curl_free() as free() if our version of curl lacks curl_free. */
+/* #undef curl_free */
